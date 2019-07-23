5f566393792a38d31557f92c6b351dabf417d957
frederico <frederico0707@gmail.com>
2019-07-23 02:02:25 -0300

    ate aula18 -o

--- src/app/pages/categories/categories.module.ts
+++ src/app/pages/categories/categories.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CategoriesRoutingModule } from './categories-routing.module';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { CategoryFormComponent } from './category-form/category-form.component';
+import {ReactiveFormsModule} from '@angular/forms';//aula 18 - sem esse import dá erro de provider. 
 
 @NgModule({
   declarations: [CategoryListComponent, CategoryFormComponent],
   imports: [
     CommonModule,
-    CategoriesRoutingModule
+    CategoriesRoutingModule,
+    ReactiveFormsModule//aula 18 - sem esse import dá erro de provider.
   ]
 })
 export class CategoriesModule { }
