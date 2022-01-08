268fd40e98c97211699c968070c929948af2bd7d
Frederico - Dev Full Stack <frederico0707gmail.com>
2022-01-08 14:22:01 -0300

    Aula 44 - Criando BaseResourceService

---
+++ src/app/shared/services/base-resource.service.ts
@@ -0,0 +1,67 @@
+import { BaseResourceModel } from "../models/base-resource.model";
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError} from "rxjs/operators";
+
+export abstract class BaseResourceService<T extends BaseResourceModel>{
+
+    protected http: HttpClient;
+    
+    constructor(protected apiPath: string){
+
+    }
+    getAll(): Observable<T[]>{
+        //o pipe é para tratar o retorno
+        return this.http.get(this.apiPath).pipe(
+            catchError(this.handleError),
+            map(this.jsonDataToResources)
+        )
+      }
+    
+      getById(id: number): Observable<T>{
+          const url = `${this.apiPath}/${id}`;
+          return this.http.get(url).pipe(
+            catchError(this.handleError),
+            map(this.jsonDataToResource)
+          )
+      }
+    
+      create(resource: T): Observable<T>{
+        return this.http.post(this.apiPath, resource).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToResource)
+        )
+      }
+    
+      update(resource: T): Observable<T>{
+        const url = `${this.apiPath}/${resource.id}`;
+        return this.http.put(url, resource).pipe(
+          catchError(this.handleError),
+          map(() => resource)
+        )
+      }
+    
+      delete(id:number):Observable<any>{
+          const url = `${this.apiPath}/${id}`;
+          return this.http.delete(url).pipe(
+            catchError(this.handleError),
+            map(() => null)
+          )
+      }
+
+    //metodos protected
+    protected jsonDataToResources(jsonData: any[]): T[]{
+        const resources: T[] = [];
+        jsonData.forEach(element => resources.push(element as T));
+        return resources;
+    }
+
+    protected handleError(error: any): Observable<any>{
+        console.log("DEU PAU NA REQUISICAO => ", error);
+        return throwError(error);
+    }
+
+    protected jsonDataToResource(jsonData: any): T{
+        return jsonData as T;
+    }
+}
