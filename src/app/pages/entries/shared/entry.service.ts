60261c42cd430e85660b3c43aab026904452a8ae
Frederico - Dev Full Stack <frederico0707gmail.com>
2022-01-09 12:39:23 -0300

    Aula 66 a 69 - Relatorios - ULTIMO COMMIT

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -4,6 +4,7 @@ import { map, catchError, flatMap} from "rxjs/operators";
 import { Entry } from './entry.model';
 import { CategoryService } from '../../categories/shared/category.service';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
+import * as moment from "moment";
 
 
 @Injectable({
@@ -44,5 +45,22 @@ export class EntryService extends BaseResourceService<Entry>{
       }),
       catchError(this.handleError)
     );
+  }
+
+  getByMonthAndYear(month: number, year: number): Observable<Entry[]>{
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries, month, year))
+    )
+  }
+
+  private filterByMonthAndYear(entries: Entry[], month: number, year: number){
+      return entries.filter(entry => {
+        const entryDate = moment(entry.date, "DD/MM/YYYY");
+        const monthMatches = entryDate.month() + 1 == month;
+        const yearMatches = entryDate.year()  == year;
+        if(monthMatches && yearMatches) {
+          return entry;
+        }
+      })
   }
 }
