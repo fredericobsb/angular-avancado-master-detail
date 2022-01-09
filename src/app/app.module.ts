2647342d934302879596acb06a9c2ee3a350426b
Frederico - Dev Full Stack <frederico0707gmail.com>
2022-01-09 09:46:48 -0300

    Aula 64 - Adiconando template da tela de relatorios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { ReportsComponent } from './pages/reports/reports/reports.component';
 
 @NgModule({
   declarations: [
